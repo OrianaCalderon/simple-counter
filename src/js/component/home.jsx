4b1dda718d559386d46dd952875d253895841076
oscq12 <orianasofia86@gmail.com>
2022-07-20 23:44:11 +0000

    counter

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,27 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container-fluid">
+			<div className="row d-flex">
+				
+				<div className="col-2 bg-dark">
+				<i className="far fa-clock fa-10x" style={{color: "solid white"}}></i>
+
+				</div>
+
+				<div className="col-2 bg-dark">ddd</div>
+
+				<div className="col-2 bg-dark">ggg</div>
+
+				<div className="col-2 bg-dark">lkh</div>
+
+				<div className="col-2 bg-dark">lkh</div>
+
+			</div>
+
+			
 		</div>
 	);
 };
---
+++ src/js/layout.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Home from "./component/home.jsx";
+
+const Layout =()=> {
+    return(
+        <>
+        <div className="container">
+
+        <Home/>
+
+        </div>
+        </>
+    )
+}
+
+export default Layout;
