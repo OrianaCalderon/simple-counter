d87758961191d58f426a6870d70ccade4e6e13bd
oscq12 <orianasofia86@gmail.com>
2022-07-21 00:28:07 +0000

    done

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 //create your first component
-const Home = () => {
+const Home = (props) => {
 	return (
 		<div className="container-fluid">
 			<div className="row d-flex">
@@ -11,17 +11,16 @@ const Home = () => {
 
 				</div>
 
-				<div className="col-2 bg-dark">ddd</div>
+				<div className="col-2 bg-dark p-5"><h1>{props.num4}</h1></div>
 
-				<div className="col-2 bg-dark">ggg</div>
+				<div className="col-2 bg-dark p-5"><h1>{props.num3}</h1></div>
 
-				<div className="col-2 bg-dark">lkh</div>
+				<div className="col-2 bg-dark p-5"><h1>{props.num2}</h1></div>
 
-				<div className="col-2 bg-dark">lkh</div>
+				<div className="col-2 bg-dark p-5"><h1>{props.num1}</h1></div>
 
 			</div>
 
-			
 		</div>
 	);
 };
--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,17 @@ import ReactDOM from "react-dom";
 import "../styles/index.css";
 
 //import your own components
-import Layout from "./layout.jsx";
+import Home from "./component/home.jsx";
 
-//render your react application
-ReactDOM.render(<Layout />, document.querySelector("#app"));
+
+let count = 0;
+
+
+setInterval( ()=> {
+    let num1 = Math.floor((count/1)%10);
+    let num2 = Math.floor((count/10)%10);
+    let num3 = Math.floor((count/100)%10);
+    let num4 = Math.floor((count/1000)%10);
+    count++
+    ReactDOM.render(<Home num1={num1} num2={num2} num3={num3} num4={num4} />, document.querySelector("#app"));
+}, 1000)
